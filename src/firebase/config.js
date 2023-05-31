19463c4d5a1624ade99aabd8de85754aa056ac13
ann-babu <annmarybabu2023@cs.ajce.in>
2023-05-31 11:21:35 +0530

    mm

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC4ZzWIMjOIs_ZYolLEpPhIGDiuMgHpuHU",
+    authDomain: "fir-dceab.firebaseapp.com",
+    projectId: "fir-dceab",
+    storageBucket: "fir-dceab.appspot.com",
+    messagingSenderId: "865993177468",
+    appId: "1:865993177468:web:93bb32d50d88ea49864cf9",
+    measurementId: "G-6G8XS929W4"
+  };
+
+  export const Firebase=firebase.initializeApp(firebaseConfig)
